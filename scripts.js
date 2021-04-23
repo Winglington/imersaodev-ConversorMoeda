6a4f36938bb0f6a8517c5918f348d9042f74ccaa
Josue Winglington <winglington@gmail.com>
2021-04-23 00:14:47 -0300

    Create scripts.js

---
+++ scripts.js
@@ -0,0 +1,5 @@
+var valorEmDollar = prompt("Digite o valor em dollar")
+valorEmDollar = parseFloat(valorEmDollar)
+var valorEmReal = valorEmDollar * 5.77
+valorEmReal = valorEmReal.toFixed(2)
+alert(valorEmReal)
